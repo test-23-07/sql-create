77210bbe2a810472fce322cca7753d194d322727
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2023-11-17 09:22:39 +0100

    Initial commit

---
+++ create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS Benutzer (
+    vorname VARCHAR(255),
+    nachname VARCHAR(255),
+    email VARCHAR(255)
+);
+
+INSERT INTO Benutzer (vorname, nachname, email) VALUES ('Hans', 'Dampf', 'hans@dampf.de');
+
+
+INSERT INTO Benutzer (vorname, nachname, email) VALUES ('Lilli', 'Wolke', 'lilli@wolke.de');
+
+
+
